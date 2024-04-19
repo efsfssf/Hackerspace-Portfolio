d8a8f8d508091eec049f204e20c3c43a1ef284ac
MrDandomi <bezzubik46@gmail.com>
2024-04-20 00:23:23 +0300

    Update AboutSection.jsx

--- src/pages/Home/AboutSection.jsx
+++ src/pages/Home/AboutSection.jsx
@@ -10,30 +10,24 @@ const AboutSection = ({ ...props }) => {
     <section className="about" {...props} data-aos="fade-up">
       <div className="titleContainer">
         <span className="tagAbout tagAbout1">{`<h2>`}</span>
-        <h1 className="title">About Us</h1>
+        <h1 className="title">About Me</h1>
         <span className="tagAbout tagAbout2">{`</h2>`}</span>
       </div>
 
       <div className="mob-aboutContent">
         <div>
-          A Community of students having similar interest in the field of
-          coding, where one can learn, implement and share new skills. Here
-          students get more exposure and get to know about the industrial
-          experiences of working seniors. Hackerspace always maintains a
-          friendly environment for students to develop new skills and go beyond
-          the boundaries
+          Hello, my nickname is Dandomi and I am an experienced developer specializing in building high quality and reliable software solutions. With a deep understanding of C# and the MVVM architectural pattern, I strive to build intuitive and scalable applications. My experience includes working with Python, JavaScript, and contributing to projects in Java, PHP, TypeScript, and Kotlin.
+However, my passion is not limited to development. I am also active in finding vulnerabilities in software and web resources, which makes me not only a developer but also an ethical hacker. My cybersecurity efforts have resulted in over 6 high level vulnerability reports on the Standoff 365 platform, which emphasizes my dedication to protecting data and systems.
+With me, you won't just get code, but a valuable partner ready to take responsibility for the security and efficiency of your software products.
         </div>
       </div>
 
       <div className="aboutContent">
         <div className="aboutText">
           <p>
-            A Community of students having similar interest in the field of
-            coding, where one can learn, implement and share new skills. Here
-            students get more exposure and get to know about the industrial
-            experiences of working seniors. Hackerspace always maintains a
-            friendly environment for students to develop new skills and go
-            beyond the boundaries
+            Hello, my nickname is Dandomi and I am an experienced developer specializing in building high quality and reliable software solutions. With a deep understanding of C# and the MVVM architectural pattern, I strive to build intuitive and scalable applications. My experience includes working with Python, JavaScript, and contributing to projects in Java, PHP, TypeScript, and Kotlin.
+However, my passion is not limited to development. I am also active in finding vulnerabilities in software and web resources, which makes me not only a developer but also an ethical hacker. My cybersecurity efforts have resulted in over 6 high level vulnerability reports on the Standoff 365 platform, which emphasizes my dedication to protecting data and systems.
+With me, you won't just get code, but a valuable partner ready to take responsibility for the security and efficiency of your software products.
           </p>
         </div>
         <div className="aboutLogo"></div>
