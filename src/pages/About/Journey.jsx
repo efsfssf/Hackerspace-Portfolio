580eea7ff80e29d48c019ca1a81e891820dea017
MrDandomi <bezzubik46@gmail.com>
2024-04-20 00:00:10 +0300

    Update Journey.jsx

--- src/pages/About/Journey.jsx
+++ src/pages/About/Journey.jsx
@@ -7,16 +7,12 @@ const Journey = ({ ...props }) => {
     <section className="journey" {...props}>
       <div className="titleContainer" data-aos="fade-up">
         <span className="tagAbout tagAbout1">{`<h2>`}</span>
-        <h1 className="title">Journey</h1>
+        <h1 className="title">Hacking</h1>
         <span className="tagAbout tagAbout2">{`</h2>`}</span>
       </div>
       <div className="aboutDescription" data-aos="fade-up">
-        Explore our Journey section, a vivid timeline of Hackerspace's
-        evolution. It captures our milestones, achievements, and defining
-        moments, reflecting our growth and relentless pursuit of knowledge. Each
-        timeline entry marks a step in our collective coding journey. Navigate
-        through this timeline to witness Hackerspace's evolution and envision
-        our future.
+        Specializing in cybersecurity, I focus my efforts on finding and documenting vulnerabilities in large companies' information systems. Using advanced techniques and tools, I identify weaknesses in companies' defenses. Over the course of my work, I have created over 6 detailed vulnerability reports, most of which are categorized as high level. I utilize the Standoff 365 platform to report vulnerabilities found, thereby securing companies' data and operations. My goal is to improve defense and security. I provide information that allows companies to respond to potential threats in a timely manner.
+My contribution in the field of cybersecurity helps prevent potential incidents of data breaches and other forms of cyberattacks, thereby increasing trust and security in the digital space.
       </div>
 
       <div className="timeline">
