0035c0fa27a0f2d8a4db9b98ab23ee73eb0e4add
MrDandomi <bezzubik46@gmail.com>
2024-04-20 10:47:23 +0300

    Update ContactSection.jsx

--- src/pages/Home/ContactSection.jsx
+++ src/pages/Home/ContactSection.jsx
@@ -4,8 +4,10 @@ import "../../css/contactSection.css";
 import { FaXTwitter } from "react-icons/fa6";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaLinkedin } from "react-icons/fa";
+import { ImTelegram } from "react-icons/im";
 import { IoMdMail } from "react-icons/io";
 import { Link } from "react-router-dom";
+import { SiOpenbugbounty } from "react-icons/si";
 import { FaYoutube } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { useDispatch } from "react-redux";
@@ -17,33 +19,25 @@ const ContactSection = ({ ...props }) => {
       <section className="contactSection" {...props} data-aos="fade-up">
         <div className="titleContainer">
           <span className="tagAbout tagAbout1">{`<h2>`}</span>
-          <h1 className="title">Connect Us</h1>
+          <h1 className="title">Connect Me</h1>
           <span className="tagAbout tagAbout2">{`</h2>`}</span>
         </div>
         <div className="contactContainer">
           <a
             className="circleIcon"
             data-icon="twitter"
-            href="https://www.youtube.com/@CodingCommunityMSITHackerspace"
+            href="https://standoff365.com/profile/Dandomi/"
             target="_blank"
           >
-            <FaYoutube />
-          </a>
-          <a
-            className="circleIcon"
-            data-icon="instagram"
-            href="https://www.instagram.com/_hackerspace_msit_/"
-            target="_blank"
-          >
-            <AiFillInstagram />
+            <SiOpenbugbounty />
           </a>
           <a
             className="circleIcon"
             data-icon="linkedin"
-            href="https://www.facebook.com/profile.php?id=100091680046825"
+            href="https://t.me/zelenka_lzt_team"
             target="_blank"
           >
-            <FaFacebook />
+            <ImTelegram />
           </a>
           <Link className="circleIcon" data-icon="mail" to="/contact">
             <IoMdMail />
